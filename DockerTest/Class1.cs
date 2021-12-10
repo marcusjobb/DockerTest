38a9b7cb0c248fd358800a89633b62b73938937d
Marcus Medina <medina.jobb@outlook.com>
2021-12-10 13:22:59 +0100

    Add project files.

---
+++ DockerTest/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace DockerTest
+{
+    public class Class1
+    {
+        public int GiveMeFive()
+        {
+            return 5;
+        }
+    }
+}
---
+++ DockerTestTests/Class1Tests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DockerTest;
+
+namespace DockerTest.Tests
+{
+    [TestClass()]
+    public class Class1Tests
+    {
+        [TestMethod()]
+        public void GiveMeFiveTest()
+        {
+            var expected = 5;
+            var actual = new Class1().GiveMeFive();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
