cf92f718f8744cf8b2f4ab3cf36aba9c3d006df8
Marcus Medina <medina.jobb@outlook.com>
2021-12-11 01:01:56 +0100

    Asp.net project added, removed old project

---
+++ ASPHellworld/Pages/Disclaimer.cshtml.cs
@@ -0,0 +1,19 @@
+﻿namespace ASPHellworld.Pages
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    public class DisclaimerModel : PageModel
+    {
+        private readonly ILogger<DisclaimerModel> _logger;
+
+        public DisclaimerModel(ILogger<DisclaimerModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
